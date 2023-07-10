df1804d65eea4a2b39b058e52638b7d63a4b93a0
kolosinho <andrew.kolosenko@gmail.com>
2023-07-10 19:19:44 +0300

    "Refresh" button displays usersList in the table

--- Projects/Notes/Sites/Notes/wwwroot/js/site.js
+++ Projects/Notes/Sites/Notes/wwwroot/js/site.js
@@ -1,12 +1,23 @@
 ﻿
-$( document ).ready(function() {
-    $( "#refresh-users-btn" ).on( 'click', function() {
+$(document).ready(function () {
+    $("#refresh-users-btn").on('click', function () {
         $.ajax({
             url: '/api/noteUser'
-        }).done(function(data) {
-            var newData = "";
-            data.users.forEach(user => newData += '<p>' + user.name + '</p>' + '<hr><br>')
-            $('#users').empty().append(newData);
+        }).done(function (data) {
+            var usersTable = '' 
+
+            $.each(data.users, function (index, user) {                
+                usersTable += '<tr><th scope="row">' + user.id + '</th>'
+                usersTable += '<td>' + user.name + '</td>'
+                usersTable += '<td>' + user.age + '</td>'
+                usersTable += '<td><input type="checkbox"></td></tr>'
+            })
+
+
+            $("#users").html(usersTable);
+
+            //data.users.forEach(user => newData += '<p>' + user.name + '</p>' + '<hr><br>')
+            //$('#users').empty().append(newData);
         });
-    });
-});
+    })
+})
