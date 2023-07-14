b67c8f71d009d7292572870f9a2de31e8af1ffe7
kolosinho <andrew.kolosenko@gmail.com>
2023-07-14 16:01:49 +0300

    Improved "DELETE" button

--- Projects/Notes/Sites/Notes/wwwroot/js/site.js
+++ Projects/Notes/Sites/Notes/wwwroot/js/site.js
@@ -1,44 +1,54 @@
 ﻿
 $(document).ready(function () {
+    var isDeteleBtnClicked = false;
     $("#refresh-users-btn").on('click', function () {
         RefreshUsersTable();
+        isDeteleBtnClicked = false;
     })
 
     $("#delete-users-btn").on('click', function () {
-        var usersIds = $("#users input:checkbox:checked").map(function(){
-            if ($(this).val() === "on") {
-                return $(this).data('user-id');
-            }
-        }).get();
-        
-        var data = {
-            usersIds: usersIds
-        };
+        if (isDeteleBtnClicked === false) {
+            //Event 1: Display checkboxes while 1st click
+            $(".delete-th").removeAttr("style");
+        } else {
+            //Event 2: Delete user(s) & Page refresh while 2nd click
+            var usersIds = $("#users input:checkbox:checked").map(function () {
+                if ($(this).val() === "on") {
+                    return $(this).data('user-id');
+                }
+            }).get();
 
-        $.ajax({
-            url: '/api/noteUser/delete-users',
-            data: JSON.stringify(data),
-            method: "DELETE",
-            contentType: 'application/json'
-        }).done(function (data) {
-            RefreshUsersTable();
-        });
+            var data = {
+                usersIds: usersIds
+            };
+
+            $.ajax({
+                url: '/api/noteUser/delete-users',
+                data: JSON.stringify(data),
+                method: "DELETE",
+                contentType: 'application/json'
+            }).done(function (data) {
+                RefreshUsersTable();
+            });
+        }
+        isDeteleBtnClicked = !isDeteleBtnClicked;
     })
+
+    function RefreshUsersTable() {
+        $.ajax({
+            url: '/api/noteUser/getall'
+        }).done(function (data) {
+            var usersTable = ''
+
+            $.each(data.users, function (index, user) {
+                usersTable += '<tr><td>' + user.id + '</td>'
+                usersTable += '<td>' + user.name + '</td>'
+                usersTable += '<td>' + user.age + '</td>'
+                usersTable += '<td class="delete-th" style="display:none"><input type="checkbox" data-user-id="' + user.id + '"></td></tr>'
+            })
+
+            $("#users").html(usersTable);
+            $(".delete-th").hide();
+        });
+    };
 })
-
-function RefreshUsersTable(){
-    $.ajax({
-        url: '/api/noteUser/getall'
-    }).done(function (data) {
-        var usersTable = ''
-
-        $.each(data.users, function (index, user) {
-            usersTable += '<tr><td>' + user.id + '</td>'
-            usersTable += '<td>' + user.name + '</td>'
-            usersTable += '<td>' + user.age + '</td>'
-            usersTable += '<td><input type="checkbox" data-user-id="' + user.id + '"></td></tr>'
-        })
-
-        $("#users").html(usersTable);
-    });
-}
